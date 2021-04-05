5859859a3651ad6c647cff32386efaae9fafbf55
Unai Elizondo <unai.elizondo@biko2.com>
2021-04-05 09:45:16 +0200

    correccion de nombre de variables

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
 var ciudades = [
     {
-        ciudad: "MADRID",
-        precios: {
+        nombreCiudad: "MADRID",
+        preciosPescados: {
             vieiras: 500,
             pulpo: 0,
             centollo: 450
         },
-        km: 800
+        distanciaKm: 800
     },
     {
-        ciudad: "BARCELONA",
-        precios: {
+        nombreCiudad: "BARCELONA",
+        preciosPescados: {
             vieiras: 450,
             pulpo: 120,
             centollo: 0
         },
-        km: 1100
+        distanciaKm: 1100
     },
     {
-        ciudad: "LISBOA",
-        precios: {
+        nombreCiudad: "LISBOA",
+        preciosPescados: {
             vieiras: 600,
             pulpo: 100,
             centollo: 500
         },
-        km: 600
+        distanciaKm: 600
     }
 ];
-function calculo(listaPescados) {
-    var pesoTotal = 0;
-    listaPescados.forEach(function (a) { pesoTotal += a.cantidad; });
-    if (pesoTotal < 0 || pesoTotal > 200) {
+function obtenerMejorDestino(listaPescados) {
+    var pesoTotalPescados = 0;
+    listaPescados.forEach(function (a) { pesoTotalPescados += a.cantidad; });
+    if (pesoTotalPescados < 0 || pesoTotalPescados > 200) {
         console.log("Error, la furgoneta solamente puede llevar 200kg y la suma del peso tiene que ser positivo!");
         return "Error, la furgoneta solamente puede llevar 200kg y la suma del peso tiene que ser positivo!";
     }
     var beneficios = [
         {
-            ciudad: "madrid",
-            beneficio: 0
+            nombreCiudad: "madrid",
+            beneficioTotal: 0
         },
         {
-            ciudad: "barcelona",
-            beneficio: 0
+            nombreCiudad: "barcelona",
+            beneficioTotal: 0
         },
         {
-            ciudad: "lisboa",
-            beneficio: 0
+            nombreCiudad: "lisboa",
+            beneficioTotal: 0
         }
     ];
     listaPescados.forEach(function (pesc) {
         ciudades.forEach(function (ciudad) {
-            beneficios.filter(function (ben) { return ben.ciudad === ciudad.ciudad.toLowerCase(); })[0].beneficio += ciudad.precios[pesc.pescado.toLowerCase()] * pesc.cantidad;
+            beneficios.filter(function (ben) { return ben.nombreCiudad === ciudad.nombreCiudad.toLowerCase(); })[0].beneficioTotal += ciudad.preciosPescados[pesc.nombrePescado.toLowerCase()] * pesc.cantidad;
             //beneficios[ciudad.ciudad.toLowerCase()]+= ciudad.precios[pesc.pescado.toLowerCase()]*pesc.cantidad;
         });
     });
     ciudades.forEach(function (ciud) {
-        var km = ciud.km;
-        var depreciacion = 1 - (km / 100) / 100;
-        var costeFurgo = 5 + km * 2;
+        var distanciaKm = ciud.distanciaKm;
+        var depreciacionPescado = 1 - (distanciaKm / 100) / 100;
+        var costeFurgo = 5 + distanciaKm * 2;
         //beneficios[ciud.ciudad.toLowerCase()] = beneficios[ciud.ciudad.toLowerCase()]*depreciacion - costeFurgo;
-        var as = beneficios.filter(function (ben) { return ben.ciudad === ciud.ciudad.toLowerCase(); })[0];
-        beneficios.filter(function (ben) { return ben.ciudad === ciud.ciudad.toLowerCase(); })[0].beneficio = as.beneficio * depreciacion - costeFurgo;
+        var beneficioCiudadX = beneficios.filter(function (ben) { return ben.nombreCiudad === ciud.nombreCiudad.toLowerCase(); })[0];
+        beneficios.filter(function (ben) { return ben.nombreCiudad === ciud.nombreCiudad.toLowerCase(); })[0].beneficioTotal = beneficioCiudadX.beneficio * depreciacionPescado - costeFurgo;
     });
     beneficios.sort(function (a, b) {
-        if (a.beneficio > b.beneficio) {
+        if (a.beneficioTotal > b.beneficioTotal) {
             return -1;
         }
-        return a.beneficio < b.beneficio ? 1 : 0;
+        return a.beneficioTotal < b.beneficioTotal ? 1 : 0;
     });
     var respuesta = "";
     beneficios.forEach(function (ben) {
-        respuesta += "Si vas a " + ben.ciudad + " tendrias el beneficio de " + ben.beneficio + " \u20AC \n";
+        respuesta += "Si vas a " + ben.nombreCiudad + " tendrias el beneficio de " + ben.beneficioTotal + " \u20AC \n";
     });
-    respuesta += "\n\nVete " + beneficios[0].ciudad + " que es donde mas vas a ganar!";
+    respuesta += "\n\nVete " + beneficios[0].nombreCiudad + " que es donde mas vas a ganar!";
     //let respuesta:string = `Si vas a Madrid tendrias el beneficio de ${beneficios.madrid} € \nSi vas a Barcelona tendrias el beneficio de ${beneficios.barcelona} € \nSi vas a Lisboa tendrias el beneficio de ${beneficios.lisboa} €`
     // let nums:number[] =[beneficios.madrid,beneficios.barcelona,beneficios.lisboa];
     console.log(respuesta);
 }
 var pescados = [{
-        pescado: "VIEIRAS",
+        nombrePescado: "VIEIRAS",
         cantidad: 50
     },
     {
-        pescado: "PULPO",
+        nombrePescado: "PULPO",
         cantidad: 100
     },
     {
-        pescado: "CENTOLLO",
+        nombrePescado: "CENTOLLO",
         cantidad: 50
     }
 ];
-calculo(pescados);
+obtenerMejorDestino(pescados);
--- index.ts
+++ index.ts
@@ -1,117 +1,117 @@
-let ciudades: {ciudad: string, precios:{vieiras:number,pulpo:number, centollo:number}, km:number}[] =  [
+let ciudades: {nombreCiudad: string, preciosPescados:{vieiras:number,pulpo:number, centollo:number}, distanciaKm:number}[] =  [
     {
-        ciudad: "MADRID",
-        precios: 
+        nombreCiudad: "MADRID",
+        preciosPescados: 
             {
                 vieiras: 500,
                 pulpo: 0,
                 centollo: 450
             }
         ,
-        km: 800
+        distanciaKm: 800
     },
     {
-        ciudad: "BARCELONA",
-        precios: 
+        nombreCiudad: "BARCELONA",
+        preciosPescados: 
             {
                 vieiras: 450,
                 pulpo: 120,
                 centollo: 0
             }
         ,
-        km: 1100
+        distanciaKm: 1100
     },
     {
-        ciudad: "LISBOA",
-        precios: 
+        nombreCiudad: "LISBOA",
+        preciosPescados: 
             {
                 vieiras: 600,
                 pulpo: 100,
                 centollo: 500
             }
         ,
-        km: 600
+        distanciaKm: 600
     }
 ];
 
-function calculo(listaPescados:any[]){
+function obtenerMejorDestino(listaPescados:any[]){
 
-    let pesoTotal: number = 0;
+    let pesoTotalPescados: number = 0;
 
-    listaPescados.forEach(function(a){pesoTotal += a.cantidad;});
+    listaPescados.forEach(function(a){pesoTotalPescados += a.cantidad;});
 
-    if(pesoTotal<0 || pesoTotal>200){
+    if(pesoTotalPescados<0 || pesoTotalPescados>200){
         console.log("Error, la furgoneta solamente puede llevar 200kg y la suma del peso tiene que ser positivo!");
         return "Error, la furgoneta solamente puede llevar 200kg y la suma del peso tiene que ser positivo!";
     }
 
-    let beneficios: {ciudad:string, beneficio:number}[] = [
+    let beneficios: {nombreCiudad:string, beneficioTotal:number}[] = [
         {
-            ciudad:"madrid",
-            beneficio: 0
+            nombreCiudad:"madrid",
+            beneficioTotal: 0
         },
         {
-            ciudad:"barcelona",
-            beneficio: 0
+            nombreCiudad:"barcelona",
+            beneficioTotal: 0
         },
         {
-            ciudad:"lisboa",
-            beneficio: 0
+            nombreCiudad:"lisboa",
+            beneficioTotal: 0
         }
 
     ];
 
     listaPescados.forEach(pesc => {
         ciudades.forEach(ciudad => {
-            beneficios.filter(ben => ben.ciudad ===ciudad.ciudad.toLowerCase())[0].beneficio += ciudad.precios[pesc.pescado.toLowerCase()]*pesc.cantidad;
+            beneficios.filter(ben => ben.nombreCiudad ===ciudad.nombreCiudad.toLowerCase())[0].beneficioTotal += ciudad.preciosPescados[pesc.nombrePescado.toLowerCase()]*pesc.cantidad;
             //beneficios[ciudad.ciudad.toLowerCase()]+= ciudad.precios[pesc.pescado.toLowerCase()]*pesc.cantidad;
         });
         
     });
 
     ciudades.forEach(ciud => {
-        let km:number = ciud.km;
-        let depreciacion = 1-(km/100)/100;
-        let costeFurgo = 5 + km*2;
+        let distanciaKm:number = ciud.distanciaKm;
+        let depreciacionPescado = 1-(distanciaKm/100)/100;
+        let costeFurgo = 5 + distanciaKm*2;
         //beneficios[ciud.ciudad.toLowerCase()] = beneficios[ciud.ciudad.toLowerCase()]*depreciacion - costeFurgo;
 
-        let as:any = beneficios.filter(ben => ben.ciudad ===ciud.ciudad.toLowerCase())[0];
-        beneficios.filter(ben => ben.ciudad ===ciud.ciudad.toLowerCase())[0].beneficio = as.beneficio*depreciacion - costeFurgo;
+        let beneficioCiudadX:any = beneficios.filter(ben => ben.nombreCiudad ===ciud.nombreCiudad.toLowerCase())[0];
+        beneficios.filter(ben => ben.nombreCiudad ===ciud.nombreCiudad.toLowerCase())[0].beneficioTotal = beneficioCiudadX.beneficio*depreciacionPescado - costeFurgo;
     });
 
 
 
     beneficios.sort((a,b)=>{
-        if(a.beneficio>b.beneficio){return -1}
-        return a.beneficio < b.beneficio ?1:0;
+        if(a.beneficioTotal>b.beneficioTotal){return -1}
+        return a.beneficioTotal < b.beneficioTotal ?1:0;
     });
 
     let respuesta:string = ``;
 
     beneficios.forEach(ben => {
-        respuesta += `Si vas a ${ben.ciudad} tendrias el beneficio de ${ben.beneficio} € \n`
+        respuesta += `Si vas a ${ben.nombreCiudad} tendrias el beneficio de ${ben.beneficioTotal} € \n`
     }); 
 
-    respuesta+= `\n\nVete ${beneficios[0].ciudad} que es donde mas vas a ganar!`;
+    respuesta+= `\n\nVete ${beneficios[0].nombreCiudad} que es donde mas vas a ganar!`;
 
     //let respuesta:string = `Si vas a Madrid tendrias el beneficio de ${beneficios.madrid} € \nSi vas a Barcelona tendrias el beneficio de ${beneficios.barcelona} € \nSi vas a Lisboa tendrias el beneficio de ${beneficios.lisboa} €`
    // let nums:number[] =[beneficios.madrid,beneficios.barcelona,beneficios.lisboa];
     console.log(respuesta);
 }
 
-let pescados: {pescado:string, cantidad: number}[] = [{
-        pescado: "VIEIRAS",
+let pescados: {nombrePescado:string, cantidad: number}[] = [{
+        nombrePescado: "VIEIRAS",
         cantidad: 50
     },
     {
-        pescado: "PULPO",
+        nombrePescado: "PULPO",
         cantidad: 100
     },
     {
-        pescado: "CENTOLLO",
+        nombrePescado: "CENTOLLO",
         cantidad: 50
     }
     
 ];
 
-calculo(pescados);
+obtenerMejorDestino(pescados);
